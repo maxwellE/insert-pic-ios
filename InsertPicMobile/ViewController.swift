ccea7b501b4b64564edeada5da90a2834706e133
Maxwell Elliott <maxwell@gumroad.com>
2014-07-12 13:34:22 -0700

    Initial Commit

---
+++ InsertPicMobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InsertPicMobile
+//
+//  Created by Maxwell Elliott on 7/12/14.
+//  Copyright (c) 2014 InsertPic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
